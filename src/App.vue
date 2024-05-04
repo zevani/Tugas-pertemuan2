37d2724aa4f62d0536e423ab42d68e6208c510ad
zevani <sy.zevanianandazellery@student.uir.ac.id>
2024-05-04 17:22:30 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <h1>Pencari Nama Buah</h1>
+    <input type="text" v-model="cari" class="input-cari" :style="{ width: ukuranInput + 'px' }" />
+    <button @click="cariKata">Cari</button>
+    <ul class="hasil-pencarian">
+      <li v-for="kata in hasilPencarian" :key="kata">{{ kata }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cari: "",
+      hasilPencarian: [],
+      ukuranInput: 200,
+    };
+  },
+  methods: {
+    cariKata() {
+      const kataCari = this.cari.toLowerCase();
+      this.hasilPencarian = ["apel","belimbing","cempedak","duku","jambu","kelengkeng","leci","mangga","nenas","pisang","rambutan"].filter(kata => kata.includes(kataCari));
+    },
+    perbesarInput() {
+      if (this.ukuranInput < 500) {
+        this.ukuranInput += 50;
+      }
+    },
+    perkecilInput() {
+      if (this.ukuranInput > 100) {
+        this.ukuranInput -= 50;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  padding: 20px;
+}
+
+.input-cari {
+  padding: 10px;
+  border: 1px solid #ccc;
+}
+
+.hasil-pencarian {
+  list-style: none;
+  padding: 0;
+}
+
+.hasil-pencarian li {
+  padding: 10px;
+  border-bottom: 1px solid #ccc;
+}
+</style>
